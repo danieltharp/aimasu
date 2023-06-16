c7ddd52adfdfee12536db625f1f5b85ebcf37512
Daniel Tharp <github@nags.me>
2023-06-16 11:20:05 -0600

    Localization work

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'これらの資格情報は、私たちの記録と一致しません。',
+    'password' => 'パスワードが間違っています。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds 秒後にもう一度お試しください。',
 
 ];
